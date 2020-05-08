01f2f4eb819245373f2b20828c99bea90a741514
anfony123 <64474462+anfony123@users.noreply.github.com>
2020-05-08 16:04:33 +0800

    user profile

--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from datetime import datetime
 from app import db
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -19,6 +20,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,3 +54,4 @@ class User(UserMixin, db.Model):
 
     def is_anonymous(self):
         return False
+
