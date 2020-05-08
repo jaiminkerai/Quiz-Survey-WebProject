7491c9b3675fee40a74e46061ee2764fe284171c
Jaimin Kerai <22718975@student.uwa.edu.au>
2020-05-08 16:17:54 +0800

    changes to return after login

--- app/routes.py
+++ app/routes.py
@@ -42,6 +42,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
