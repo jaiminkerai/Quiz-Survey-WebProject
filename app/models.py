962944140f241d4848b961eb80218972ea55d13e
Jaimin Kerai <22718975@student.uwa.edu.au>
2020-05-08 16:17:27 +0800

    UserMixin

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 from flask_login import UserMixin
 from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -32,23 +32,3 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
-class User(UserMixin, db.Model):
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
-    def is_active(self):
-        return True
-
-    def get_id(self):
-        return self.email
-
-    def is_authenticated(self):
-        return self.authenticated
-
-    def is_anonymous(self):
-        return False
