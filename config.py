56fd4db82fc941791f0c12148d57c2d91855cdd8
Anthony Huynh <22716758@student.uwa.edu.au>
2020-05-12 14:09:10 +0800

    explore page and limiting posts

--- config.py
+++ config.py
@@ -14,3 +14,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+
+    #Posts per page configuration
+    POSTS_PER_PAGE = 3
+    
