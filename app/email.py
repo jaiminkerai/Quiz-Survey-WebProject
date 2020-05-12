ae19b2f7766e0081d0a262bf24f20bbcb775da91
Jaimin Kerai <22718975@student.uwa.edu.au>
2020-05-12 18:13:09 +0800

    for email capabilities

--- app/email.py
+++ app/email.py
@@ -6,7 +6,7 @@ from app import mail
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Quizzards] Reset Your Password',
+    send_email('[Quizards] Reset Your Password',
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
