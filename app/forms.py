de7708d27086347e1f4d1d64a027bcb37695cb98
Jayden <jayden228@gmail.com>
2020-05-19 10:14:03 +0800

    added edit email in edit profile form

--- app/forms.py
+++ app/forms.py
@@ -30,6 +30,7 @@ class RegistrationForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
