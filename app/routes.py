1e0a97463534878300c02457cf1abd02655a59ca
Jayden <jayden228@gmail.com>
2020-05-19 10:13:29 +0800

    Added edit email it edit profile route

--- app/routes.py
+++ app/routes.py
@@ -120,14 +120,20 @@ def before_request():
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
+
+    # Change current user fields based on form input post
     if form.validate_on_submit():
         current_user.username = form.username.data
+        current_user.email = form.email.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
+    
+    # Show current account details
     elif request.method == 'GET':
         form.username.data = current_user.username
+        form.email.data = current_user.email
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
