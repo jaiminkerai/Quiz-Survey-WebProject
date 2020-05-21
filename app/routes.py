48758978785896046f24f3b48d09a9fb2c18a2a7
Jayden <jayden228@gmail.com>
2020-05-21 14:22:46 +0800

    added created quizzes to user route

--- app/routes.py
+++ app/routes.py
@@ -112,16 +112,28 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    # User Object
     user = User.query.filter_by(username=username).first_or_404()
+
+    # Pagination Variable
     page = request.args.get('page', 1, type=int)
+
+    # Posts of User
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+
+    # Quizzes of User
+    quizzes = user.authorOf.paginate(page)
+
+    # Pagination
     next_url = url_for('user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
+
+    # Render the HTML Template
     return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+                           next_url=next_url, prev_url=prev_url, quizzes=quizzes.items)
 
 @app.before_request
 def before_request():
